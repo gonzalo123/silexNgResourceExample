33105235a32f047c97d181410a4d5e08a6f8b043
Gonzalo Ayuso <gonzalo123@gmail.com>
2013-05-26 14:54:56 +0200

    clean

--- api/index.php
+++ api/index.php
@@ -2,28 +2,33 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\YamlFileLoader;
 use Symfony\Component\Routing\RouteCollection;
 use Silex\Application;
 
-$app = new Silex\Application();
+$app          = new Silex\Application();
 $app['debug'] = true;
 
-$app['routes'] = $app->extend('routes', function (RouteCollection $routes, Application $app) {
-    $loader     = new YamlFileLoader(new FileLocator(__DIR__ . '/config'));
-    $collection = $loader->load('routes.yml');
-    $routes->addCollection($collection);
+$app['routes'] = $app->extend(
+    'routes',
+    function (RouteCollection $routes, Application $app) {
+        $loader     = new YamlFileLoader(new FileLocator(__DIR__ . '/config'));
+        $collection = $loader->load('routes.yml');
+        $routes->addCollection($collection);
 
-    return $routes;
-});
+        return $routes;
+    }
+);
 
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => array(
-        'driver' => 'pdo_sqlite',
-        'path'   => __DIR__ . '/db/app.db.sqlite',
-    ),
-));
+$app->register(
+    new Silex\Provider\DoctrineServiceProvider(),
+    array(
+        'db.options' => array(
+            'driver' => 'pdo_sqlite',
+            'path'   => __DIR__ . '/db/app.db.sqlite',
+        ),
+    )
+);
 
 $app->run();
